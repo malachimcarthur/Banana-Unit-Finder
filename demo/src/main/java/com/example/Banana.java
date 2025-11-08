561a6d1de9245924b077062edc46658fb0d549a4
Carter-Cr <ccrist11@huskers.unl.edu>
2025-11-08 15:03:03 -0600

    e b.updat

--- demo/src/main/java/com/example/Banana.java
+++ demo/src/main/java/com/example/Banana.java
@@ -1,34 +1,34 @@
 package com.example;
 
 public class Banana{
-    public Banana(){
-
+    	private static final double MICROSIVERTS_PER_INCH = 0.0111428571;
+	private double a,b;
+	private double sizeCubicInch;
+	private double microsiverts;
+    public Banana(double a, double b){
+    	this.a = a;
+    	this.b = b;
     }
     /**
      * Gets the volume of an object assuming its a cylander.
      */
-    public static double Volume(double a, double b){
-        return (.5 * (getTerm(b) - getTerm(a))) * 2;
+    public void Volume(){
+    	double volume, t1, t2;
+    	t1 = this.b * Math.PI;
+    	t2 = this.a * Math.PI;
+    	volume = t1 - t2;
+    	volume *= 0.5;
+    	//volume *= 2;
+    	this.sizeCubicInch = volume;
+        //return (.5 * (getTerm(b) - getTerm(a))) * 2;
     }
-
-    /**
-     * Gets the radius.
-     */
-    public static double getR(double x){
-        return (1/2) * (((.1/3) * Math.pow(x,3)) - (((.1/3) * Math.pow(x,3)) - (2 * x)));
+    
+    public double toOunce() {
+    	return this.sizeCubicInch * 0.57674377;
     }
-
-    /**
-     * Squares the radius.
-     */
-    public static double getRSquared(double x){
-        double r = getR(x);
-        return r*r;
-    }
-    /**
-     * Gets the full term of b or a.
-     */
-    public static double getTerm(double x){
-        return Math.PI * getRSquared(x);
+    
+    public double microsivertsPerInch() {
+    	double length = Math.abs(a-b);
+    	return length * MICROSIVERTS_PER_INCH;
     }
 }
